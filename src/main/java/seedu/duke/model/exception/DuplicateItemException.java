f8a9b3a0b8739c985fff20ef3eb70ac0c0455c6a
Junfeng <yuejunfeng0909@gmail.com>
2021-10-11 13:03:43 +0800

    Minor format change

--- src/main/java/seedu/duke/model/exception/DuplicateItemException.java
+++ src/main/java/seedu/duke/model/exception/DuplicateItemException.java
@@ -1,7 +1,9 @@
 package seedu.duke.model.exception;
 
 public class DuplicateItemException extends RuntimeException {
-    static public final String MESSAGE_DUPLICATE_ITEM = "The item %s already exists";
+
+    public static final String MESSAGE_DUPLICATE_ITEM = "The item %s already exists";
+
     public DuplicateItemException(String nameOfDuplicateItem) {
         super(String.format(MESSAGE_DUPLICATE_ITEM, nameOfDuplicateItem));
     }
