48c6f6fc3071fc2ee524d7f519874f1e96fe506b
Zhihuang Wang <e0543682@u.nus.edu>
2021-10-14 17:33:37 +0800

    Enhance ListCommand and GetCommand to show all attributes

--- src/main/java/seedu/duke/command/ListCommand.java
+++ src/main/java/seedu/duke/command/ListCommand.java
@@ -1,8 +1,32 @@
 package seedu.duke.command;
 
+import seedu.duke.model.Item;
 import seedu.duke.model.ItemContainer;
 
 public class ListCommand extends Command {
+
+    /**
+     * Retrieves list of items from ItemContainer.
+     *
+     * @param list the ItemContainer in which lsit is retrieved
+     * @return list of items
+     */
+    private String getList(ItemContainer list) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < list.getSize();  i++) {
+            Item selectedItem = list.getItem(i);
+            result.append(i + 1).append(". ")
+                    .append(selectedItem.getName())
+                    .append(" (purchase cost: ")
+                    .append(selectedItem.getPurchaseCost())
+                    .append(", selling price: ")
+                    .append(selectedItem.getSellingPrice())
+                    .append(")")
+                    .append("\n");
+        }
+
+        return result.toString().trim();
+    }
 
     /**
      * Executes the operation of listing all the items.
@@ -11,7 +35,8 @@ public class ListCommand extends Command {
      */
     public void execute(ItemContainer list) {
         int initialSize = list.getSize();
-        System.out.println(list);
+        String result = getList(list);
+        System.out.println(result);
         assert initialSize == list.getSize() : "List size should not be changed";
     }
 }
