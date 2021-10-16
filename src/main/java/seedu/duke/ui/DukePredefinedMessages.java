5029812d2e2665b4ee78743b8ce05712ba37c1f7
Junfeng <yuejunfeng0909@gmail.com>
2021-10-16 22:30:58 +0800

    Update welcome message logo

--- src/main/java/seedu/duke/ui/DukePredefinedMessages.java
+++ src/main/java/seedu/duke/ui/DukePredefinedMessages.java
@@ -6,11 +6,13 @@ public class DukePredefinedMessages {
      * Print the welcome message with Duke LOGO in a message bubble.
      */
     public static void printWelcomeMessage() {
-        final String dukeLogo = " ____        _\n"
-            + "|  _ \\ _   _| | _____\n"
-            + "| | | | | | | |/ / _ \\\n"
-            + "| |_| | |_| |   <  __/\n"
-            + "|____/ \\__,_|_|\\_\\___|\n";
+        final String dukeLogo
+             = "  _____ _      _____              _____ _          _  __\n"
+             + " / ____| |    |_   _|            / ____| |        | |/ _|\n"
+             + "| |    | |      | |_   _____ _ _| (___ | |__   ___| | |_\n"
+             + "| |    | |      | \\ \\ / / _ \\ '__\\___ \\| '_ \\ / _ \\ |  _|\n"
+             + "| |____| |____ _| |\\ V /  __/ |  ____) | | | |  __/ | | \n"
+             + " \\_____|______|_____\\_/ \\___|_| |_____/|_| |_|\\___|_|_| \n";
         MessageBubble.printMessageBubble("Hello from\n" + dukeLogo + "What can I do for you?");
     }
 
