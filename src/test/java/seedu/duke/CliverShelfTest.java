3ca5ede863ad502860584dae2227470ca25881ce
Junfeng <yuejunfeng0909@gmail.com>
2021-10-28 23:32:59 +0800

    Rename Duke to CliverShelf

--- src/test/java/seedu/duke/CliverShelfTest.java
+++ src/test/java/seedu/duke/CliverShelfTest.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-class DukeTest {
+class CliverShelfTest {
 
     @Test
     public void sampleTest() {
