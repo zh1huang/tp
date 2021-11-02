60e64fcaf8d80d7e683883a2ff043c3ec131d436
Junfeng <yuejunfeng0909@gmail.com>
2021-11-02 12:31:10 +0800

    Remove auto remove data for windows

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -17,5 +17,3 @@ java -jar %jarloc% < ..\..\text-ui-test\input.txt > ..\..\text-ui-test\ACTUAL.TX
 cd ..\..\text-ui-test
 
 FC ACTUAL.TXT EXPECTED.TXT >NUL && ECHO Test passed! || Echo Test failed!
-
-del data\Data.txt
