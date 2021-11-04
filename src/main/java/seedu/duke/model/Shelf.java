6b45235dbe98e27349eb5a2a340d3a126ca90d91
Yue Junfeng <yuejunfeng0909@gmail.com>
2021-11-04 20:50:45 +0800

    Sort by alphabetical order, ignore case

--- src/main/java/seedu/duke/model/Shelf.java
+++ src/main/java/seedu/duke/model/Shelf.java
@@ -293,7 +293,7 @@ public class Shelf {
     }
 
     private void sortItems() {
-        Comparator<Item> byName = (Item o1, Item o2) -> o1.getName().compareTo(o2.getName());
+        Comparator<Item> byName = Comparator.comparing((Item o) -> o.getName().toLowerCase());
         items.sort(byName);
     }
 
