852642b0ddcb5619016d79aa852ebca604e1ff9c
Yue Junfeng <yuejunfeng0909@gmail.com>
2021-11-04 22:29:09 +0800

    Minor changes

--- src/main/java/seedu/duke/command/PrintDummyIdCommand.java
+++ src/main/java/seedu/duke/command/PrintDummyIdCommand.java
@@ -5,12 +5,12 @@ import seedu.duke.model.Item;
 import seedu.duke.model.exception.IllegalModelArgumentException;
 import seedu.duke.model.exception.ShelfNotExistException;
 
-public class PrintDummyIdCommand extends Command{
+public class PrintDummyIdCommand extends Command {
     public static final String DUMMY_ID_STRING = "dummyid";
 
     @Override
     public String execute() throws CommandException, ShelfNotExistException, IllegalModelArgumentException {
-        Item.printDummyID(true);
+        Item.setPrintDummyID(true);
         return "Dummy ID will be printed instead of actual ID";
     }
 }
