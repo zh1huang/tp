d06d0d4c63b9cacc3bed7bd91b19498dc0f943cf
Zhang Haoyu <haoyusimon927@gmail.com>
2021-11-08 13:14:12 +0800

    Update author tag

--- src/main/java/seedu/duke/logic/command/sales/SalesReport.java
+++ src/main/java/seedu/duke/logic/command/sales/SalesReport.java
@@ -97,7 +97,6 @@ public class SalesReport {
         return stringToReturn;
     }
 
-    //@@author haoyusimon
     /**
      * Generate and gets every sold item details in the soldItems shelf.
      *
