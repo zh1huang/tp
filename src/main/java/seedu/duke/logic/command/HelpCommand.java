785506ced0371415e4544ee8454fb45f07aceac7
Zhihuang Wang <e0543682@u.nus.edu>
2021-11-08 11:52:42 +0800

    Fix inconsistencies in help

--- src/main/java/seedu/duke/logic/command/HelpCommand.java
+++ src/main/java/seedu/duke/logic/command/HelpCommand.java
@@ -17,12 +17,12 @@ public class HelpCommand extends Command {
             + "4. Add item: add n/NAME shlv/SHELF_NAME p/PURCHASE_PRICE s/SELLING_PRICE q/QUANTITY [r/REMARKS]\n"
             + "5. Delete item: delete shlv/SHELF_NAME i/INDEX\n"
             + "6. List items: list [shlv/SHELF_NAME]\n"
-            + "7. Search item: search KEYWORD\n"
+            + "7. Search item: search k/KEYWORD\n"
             + "8. Get information of item : get shlv/SHELF_NAME i/INDEX\n"
             + "9. Edit an item: edit shlv/SHELF_NAME i/INDEX p/PROPERTY v/VALUE \n"
-            + "10. Sell an item: sell shlv/SHELF_NAME i/INDEX\n"
+            + "10. Sell an item: sell id/ITEM_ID\n"
             + "11. Markup price of item: markup shlv/SHELF_NAME i/INDEX [%/PERCENT_MARKUP]\n"
-            + "12. Generate sales report: report t/TYPE ym/START-YEAR-MONTH [ym/END-YEAR-MONTH]\n"
+            + "12. Generate sales report: report t/CONTENT_TYPE ym/START-YEAR-MONTH [ym/END-YEAR-MONTH]\n"
             + "13. Exit program: bye\n";
 
     /**
